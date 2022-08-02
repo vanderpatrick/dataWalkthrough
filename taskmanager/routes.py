174517a64639124eacb7c71c7288da0679ac0153
vanderpatrick <patrick123321123@icloud.com>
2022-08-02 13:34:31 +0000

    Add categorie and add_categories html pages, create render function for categories.html

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,3 +6,8 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
