a227036149093f62e4a0ea189c39c3680e9381ae
vanderpatrick <patrick123321123@icloud.com>
2022-08-03 09:52:41 +0000

    Final commit

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,6 +7,11 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+if.os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
