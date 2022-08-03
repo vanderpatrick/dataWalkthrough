2f61996c37da51ef21e142817d177e204aa25214
vanderpatrick <patrick123321123@icloud.com>
2022-08-03 08:29:46 +0000

    Add delete functionality to the routes.py

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -21,7 +21,8 @@ def add_category():
         db.session.add(category)
         db.session.commit()
         return redirect(url_for("categories"))
-    return render_template("add_categories.html")
+    return render_template("add_category.html")
+
 
 @app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
@@ -31,3 +32,11 @@ def edit_category(category_id):
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category():
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
