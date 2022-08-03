553acb5836fb51971c4aee08dc30d688f4eda153
vanderpatrick <patrick123321123@icloud.com>
2022-08-03 09:03:24 +0000

    add task functionality to routes.py

--- taskmanager/static/css/js/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
-    var sidenav = document.querySelectorAll('.sidenav');
-     M.Sidenav.init(sidenav);
-  });
